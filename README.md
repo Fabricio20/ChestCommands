dba500f88b4430152a82b6cbe5a4d216929c9947
Fabricio20 <fabricio20r@hotmail.com>
2017-12-13 18:05:27 -0200

    Update to 1.12

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ Chest Commands
 ===================
 
 Bukkit Page: http://dev.bukkit.org/bukkit-plugins/chest-commands
+
+This is an updated version of ChestCommands which makes it compatible with 1.12 for 
+replacement while I fix HubBasics.
+
+NO new features were added, it's just a compatibility fix.
+
+To compile, use `mvn -U clean package`. (Also check the releases page if you need)
+
+The source code is 100% original, with just some minor changes for compatibility, I noticed it's extremely
+"bad-written" (static abuse breaks the "abuse" barrier) but it worked in the past, so it's ok.
---
+++ src/main/java/com/gmail/filoghost/chestcommands/bridge/bungee/BungeeCordUtils.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) Fabricio20 2017 - All Rights Reserved.
+ * Created by Fabricio20 on 2017-12-13.
+ */
+package com.gmail.filoghost.chestcommands.bridge.bungee;
+
+        import java.io.ByteArrayOutputStream;
+        import java.io.DataOutputStream;
+
+        import org.bukkit.ChatColor;
+        import org.bukkit.entity.Player;
+        import com.gmail.filoghost.chestcommands.ChestCommands;
+
+public class BungeeCordUtils {
+
+    public static boolean connect(Player player, String server) {
+        try {
+            if(server.length() == 0) {
+                player.sendMessage(ChatColor.RED + "Target server was \"\" (empty string) cannot connect to it.");
+                return false;
+            }
+            ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
+            DataOutputStream out = new DataOutputStream(byteArray);
+            out.writeUTF("Connect");
+            out.writeUTF(server); // Target Server.
+            player.sendPluginMessage(ChestCommands.getInstance(), "BungeeCord", byteArray.toByteArray());
+        } catch (Exception ex) {
+            player.sendMessage(ChatColor.RED + "An unexpected exception has occurred. Please notify the server's staff about this. (They should look at the console).");
+            ex.printStackTrace();
+            ChestCommands.getInstance().getLogger().warning("Could not connect \"" + player.getName() + "\" to the server \"" + server + "\".");
+            return false;
+        }
+        return true;
+    }
+
+}
